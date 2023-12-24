3212363f04887f3553b8e54dc41f55d64914f8ba
Candela Stillo <mmcandestillo@gmail.com>
2023-12-24 16:23:09 -0300

    Second commit

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 
 const ItemListContainer = ({greeting}) => {
+
+  const productos = [
+    {titulo: "Producto A", descripcion: "descripcion de Producto A", precio: 1000},
+    {titulo: "Producto B", descripcion: "descripcion de Producto B", precio: 2000},
+    {titulo: "Producto C", descripcion: "descripcion de Producto C", precio: 3000}
+  ]
+
+  const mostrarProdcutos = new Promise((resolve, reject) =>{
+    if (productos.length > 0){
+      setTimeout(()=> {
+        resolve(productos)
+      }, 3000)
+    } else {
+      reject("No se obtuvieron productos")
+    }
+  })
+  mostrarProdcutos
+    .then((resultado) => {
+      console.log(resultado)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
   return (
     <div>{greeting}</div>
   )
